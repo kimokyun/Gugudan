64770ff7acbf9ee61b289025ce322505e68205de
pinkyun <pinkyun@kimokyun>
2018-01-16 06:20:53 -0800

    6,7단 for,while문

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -48,19 +48,31 @@ public class Gugudan {
 //		System.out.println(result);
 		
 		
-		System.out.println("구구단을 출력할 단은?");
+//		System.out.println("구구단을 출력할 단은?");
+//		
+//		Scanner scanner=new Scanner(System.in);
+//		int number=scanner.nextInt();
+//		System.out.println(number *1);
+//		System.out.println(number *2);
+//		System.out.println(number *3);
+//		System.out.println(number *4);
+//		System.out.println(number *5);
+//		System.out.println(number *6);
+//		System.out.println(number *7);
+//		System.out.println(number *8);
+//		System.out.println(number *9);
 		
-		Scanner scanner=new Scanner(System.in);
-		int number=scanner.nextInt();
-		System.out.println(number *1);
-		System.out.println(number *2);
-		System.out.println(number *3);
-		System.out.println(number *4);
-		System.out.println(number *5);
-		System.out.println(number *6);
-		System.out.println(number *7);
-		System.out.println(number *8);
-		System.out.println(number *9);
+		//6단
+//		int i=1;
+//		while(i<10) {
+//			System.out.println(6*i);
+//			i=i+1;
+//			
+//		}
+		//7단
+		for(int j=1;j<10;j++) {
+			System.out.println(7*j);
+		}
 		
 	}
 	
