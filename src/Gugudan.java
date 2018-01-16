7ea82c8cbba2d2d0c637c23467b2801c96d40f2d
pinkyun <pinkyun@kimokyun>
2018-01-16 09:43:40 -0800

    클래스 메소드

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -14,36 +14,10 @@ public class Gugudan {
 		}
 	}
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		for(int i=2;i<10;i++) {
-			int[]result=caculate(i);
-			print(result);
-		}
-		
-//		int[]result=caculate(2);
-//		print(result);
-//		
-//		int[]times3=caculate(3);
-//		print(times3);
-//		
-//		int[]times4=caculate(4);
-//		print(times4);
-	
-//		int[] result=new int[9];
-//		for(int i=0;i<result.length;i++) 
-//			result[i]=2*(i+1);
-//		
-//		for(int i=0;i<result.length;i++) {
-//			System.out.println(result[i]);
-//		
-//		}
-		
-//		
 
 		
 
 	}
 	
 
-}
+
---
+++ src/GugudanMain.java
@@ -0,0 +1,11 @@
+
+public class GugudanMain {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=2;i<10;i++) {
+			int[]result=Gugudan.caculate(i);
+			Gugudan.print(result);
+		}		
+	}		
+
+}
