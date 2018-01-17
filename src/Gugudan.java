5771231fe14cdff5c992c204470df6d7914f140c
pinkyun <pinkyun@kimokyun>
2018-01-17 08:49:16 -0800

    , 로 나눠서 구구단 만들기

--- src/Gugudan.java
+++ src/Gugudan.java
@@ -1,12 +1,15 @@
 
 public class Gugudan {
 	
-	public static int[] caculate(int times) {
-		int[] result=new int[9];
-		for(int i=0;i<result.length;i++) 
-			result[i]=times*(i+1);
+	public static void caculate(int first,int second) {
+		int result = 0;
+		for(int i=2;i<=first;i++) {
+			for(int j=1;j<=second;j++) {
+				result=i*j;
+				System.out.println(i+"*"+j+"="+result);
+			}
+		}
 		
-		return result;
 	}
 	public static void print(int[] result) {
 		for(int i=0;i<result.length;i++) {
--- src/GugudanMain.java
+++ src/GugudanMain.java
@@ -1,10 +1,23 @@
+import java.util.Scanner;
 
 public class GugudanMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		for(int i=2;i<10;i++) {
-			int[]result=Gugudan.caculate(i);
-			Gugudan.print(result);
+			//int[]result=Gugudan.caculate(i);
+			//Gugudan.print(result);
+			//int n=scanner.nextInt();
+			//Gugudan.caculate(n);
+		
+			Scanner scanner=new Scanner(System.in);
+			String inputValue=scanner.nextLine();
+			String[] splitedValue=inputValue.split(",");
+			int first=Integer.parseInt(splitedValue[0]);
+			int second=Integer.parseInt(splitedValue[1]);
+			Gugudan.caculate(first, second);
+			
+			
+			
 		}		
 	}		
 
